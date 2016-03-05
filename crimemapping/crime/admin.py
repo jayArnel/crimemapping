187a60e600ece52d0f5b0d1aab8452d45e2b0331
Jay Arnel Bilocura <jay.arnel@gmail.com>
2016-03-05 23:46:22 +0800

    updated admin of Crime model

--- crimemapping/crime/admin.py
+++ crimemapping/crime/admin.py
@@ -2,4 +2,15 @@ from django.contrib import admin
 
 from crimemapping.crime import models
 
-admin.site.register(models.Crime)
+
+class CrimeAdmin(admin.ModelAdmin):
+    list_display = (
+        'primary_type', 'crime_description', 'location_description', 'date',
+        'latitude', 'longitude')
+    search_fields = (
+        'primary_type', 'crime_description', 'location_description', 'date',
+        'latitude', 'longitude')
+    list_filter = ('date',)
+    date_hierarchy = 'date'
+
+admin.site.register(models.Crime, CrimeAdmin)
