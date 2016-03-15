344fc9aad2c10c1f1641b2a852f27562ff0b0b4e
Jay Arnel Bilocura <jay.arnel@gmail.com>
2016-03-15 14:42:39 +0800

    created CityBorder model

--- crimemapping/map/models.py
+++ crimemapping/map/models.py
@@ -1,5 +1,22 @@
-from __future__ import unicode_literals
+# This is an auto-generated Django model module created by ogrinspect.
+from django.contrib.gis.db import models
 
-from django.db import models
 
-# Create your models here.
+class CityBorder(models.Model):
+    objectid = models.IntegerField()
+    name = models.CharField(max_length=25)
+    shape_area = models.FloatField()
+    shape_len = models.FloatField()
+    geom = models.MultiPolygonField(srid=4326)
+
+    def __str__(self):
+        return self.name
+
+# Auto-generated `LayerMapping` dictionary for CityBorder model
+cityborder_mapping = {
+    'objectid': 'OBJECTID',
+    'name': 'NAME',
+    'shape_area': 'SHAPE_AREA',
+    'shape_len': 'SHAPE_LEN',
+    'geom': 'MULTIPOLYGON',
+}
