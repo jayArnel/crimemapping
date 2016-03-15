ded31c5f4759188887f72c58b1af8998ebc80e61
Jay Arnel Bilocura <jay.arnel@gmail.com>
2016-03-15 17:32:53 +0800

    updated api resource of CityBorder to include geojson attribute

--- crimemapping/map/api.py
+++ crimemapping/map/api.py
@@ -1,8 +1,12 @@
 from tastypie.resources import ModelResource
+from tastypie import fields
 from crimemapping.map.models import CityBorder
 
 
 class CityBorderResource(ModelResource):
+
+    geojson = fields.CharField(attribute='geojson', readonly=True)
+
     class Meta:
         queryset = CityBorder.objects.all()
         resource_name = 'cityborder'
