223f0d8aece430d00553dbe381bbad4bc9faef98
Jay Arnel Bilocura <jay.arnel@gmail.com>
2016-03-19 19:12:57 +0800

    updated map js to fetch data of Chicago city border from database

--- crimemapping/map/static/js/map.js
+++ crimemapping/map/static/js/map.js
@@ -2,37 +2,28 @@ require([
     'model',
     'async!https://maps.googleapis.com/maps/api/js?v=3.24&libraries=geometry&sensor=false'
 ], function(Model) {
-        /*
+    /*
       Initialize variables
      */
     var map = new google.maps.Map(document.getElementById('map'));
-    var boundariesGeoJSON = 'https://gist.githubusercontent.com/anonymous/0b1f9831d6d4941206f7/raw/b1c4dd632b4466e596169639dcba3bebda693d4e/map.geojson'
-    // Chicago City box points
+
+    // // Chicago City box points
     var NorthEast = new google.maps.LatLng(42.023135, -87.523661);
     var NorthWest = new google.maps.LatLng(42.023135, -87.940101);
     var SouthWest = new google.maps.LatLng(41.644286, -87.940101 );
     var SouthEast = new google.maps.LatLng(41.644286, -87.523661);
-    var grid_size = 1000;
-    var rectArr = [];
     var Crimes = new Model('crime');
+    var CityBorder = new Model('cityborder');
     /*
       Set initial map properties
      */
     function initializeMap() {
-      var fit = new google.maps.LatLngBounds(SouthEast, NorthEast);
-      map.fitBounds(fit);
-      map.data.loadGeoJson(boundariesGeoJSON, null, function(feat){
-        boundary = feat[0].getGeometry();
-        citybounds = new google.maps.Polygon({paths:boundary.getAt(0).getArray()})
-        map.data.setStyle(function(feature) {
-            var color = 'red';
-            var fill = 'gray';
-            return /** @type {google.maps.Data.StyleOptions} */({
-                fillColor: fill,
-                strokeColor: color,
-                strokeWeight: 2,
-            });
-        });
+        var fit = new google.maps.LatLngBounds(SouthEast, NorthEast);
+        map.fitBounds(fit);
+        CityBorder.objects.filter({'name': 'Chicago'}, function(data){
+            var chicago = data[0];
+            var feat = map.data.addGeoJson(JSON.parse(chicago.geojson))[0]; 
+        })
         Crimes.objects.filter({}, function (data){
             for (var i = 0; i < data.length; i++) {
                 var crime = data[i];
@@ -45,8 +36,7 @@ require([
                   });
             }
         });
-        drawGrid();
-      });
+        // drawGrid();
     }
     initializeMap();
 
