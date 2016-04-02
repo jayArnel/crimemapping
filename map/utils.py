f1e5620ca35826c53ef6a43c396baee2e4baaf96
Jay Arnel Bilocura <jay.arnel@gmail.com>
2016-04-02 19:43:56 +0800

    manage imports

--- map/utils.py
+++ map/utils.py
@@ -1,6 +1,6 @@
-import geopy
-from geopy import distance
 import yaml
+
+from geopy import distance, Point
 
 from django.contrib.gis import geos
 
@@ -8,7 +8,7 @@ from django.contrib.gis import geos
 def getNextPoint(point, offset, bearing):
     lat = point.y
     lon = point.x
-    pnt = geopy.Point(latitude=lat, longitude=lon)
+    pnt = Point(latitude=lat, longitude=lon)
     d = distance.VincentyDistance(kilometers=offset)
     newPnt = d.destination(point=pnt, bearing=bearing)
     return geos.Point(newPnt.longitude, newPnt.latitude)
