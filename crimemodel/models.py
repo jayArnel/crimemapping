44fb82d7bee97306f3dcd217378ce0a3a96ceb86
Jay Arnel Bilocura <jay.arnel@gmail.com>
2016-04-03 19:55:44 +0800

    fixed wrong relation use in Crime model

--- crimemodel/models.py
+++ crimemodel/models.py
@@ -2,8 +2,9 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-from crime import models
+from crime.models import Crime
+from map.models import Grid
 
 
-class Crime(models.Crime):
-    grid = models.ForeignKey('Grid', related_name='crimes')
+class Crime(Crime):
+    grid = models.ForeignKey(Grid, related_name='crimes')
