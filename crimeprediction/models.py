888c0f8ef6eb36a6d380e235c0439a7f660f5664
Jay Arnel Bilocura <jay.arnel@gmail.com>
2016-04-06 23:32:00 +0800

    created CrimeSample model

--- crimeprediction/models.py
+++ crimeprediction/models.py
@@ -2,4 +2,10 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+from crime.models import CriminalRecord
+from map.models import Grid
+
+
+class CrimeSample(models.Model):
+    crime = models.OneToOneField(CriminalRecord, related_name='sample')
+    grid = models.ManyToManyField(Grid, related_name='grids')
