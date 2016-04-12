933b094e182e3d6c4cac2a9f377b7bc0201900bc
Jay Arnel Bilocura <jay.arnel@gmail.com>
2016-04-12 22:06:24 +0800

    added static context processor to templates in settings

--- crimemapping/settings.py
+++ crimemapping/settings.py
@@ -67,6 +67,7 @@ TEMPLATES = [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.static',
                 'django.contrib.messages.context_processors.messages',
             ],
         },
