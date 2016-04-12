d18ba21c7e577dcd43049a1f0928172ab2c07772
Jay Arnel Bilocura <jay.arnel@gmail.com>
2016-04-12 22:03:18 +0800

    created gulpfile.js to build css

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp       = require('gulp');
+var sass       = require('gulp-sass');
+var watch      = require('gulp-watch');
+var plumber    = require('gulp-plumber');
+var autoprefixer = require('gulp-autoprefixer');
+
+var paths = {
+    watch: [
+        'map/static/css/*.scss',
+        ],
+    style: 'storage/static/css/base.scss',
+    base: 'storage/static',
+    build: 'map/static/build'
+}
+
+
+/* Task to build css from scss files */
+gulp.task('buildcss', function() {
+  gulp.src(paths.style, {base: paths.base})
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(autoprefixer({ browsers: ['last 2 versions'] }))
+    .pipe(gulp.dest(paths.build));
+});
+
+/* Task to watch sass changes */
+gulp.task('watch', function() {
+  gulp.watch(paths.watch,
+             ['buildcss']);
+});
+
+/* Task when running `gulp` from terminal */
+// gulp.task('default', ['compile-less', 'imagemin', 'watch-less']);
+// gulp.task('default', ['compile-less', 'watch']);
