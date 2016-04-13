2b647aad97aaa7f3460661890e086833e132f12a
Jay Arnel Bilocura <jay.arnel@gmail.com>
2016-04-13 22:59:33 +0800

    added goog and propertyParser to require config

--- map/static/js/lib/require-config.js
+++ map/static/js/lib/require-config.js
@@ -9,6 +9,8 @@
     paths: {
         jquery: 'lib/jquery-1.11.3.min',
         async: 'lib/plugins/async',
+        goog: 'lib/plugins/goog',
+        propertyParser: 'lib/plugins/propertyParser',
         stapes: 'lib/stapes-0.8.1-min',
         model: 'model',
     }
