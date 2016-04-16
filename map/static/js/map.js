2f37c7318cfed77575469f7612d2efae6c34ffac
Jay Arnel Bilocura <jay.arnel@gmail.com>
2016-04-16 17:05:32 +0800

    remove initial set date and dynamically handle select and range

--- map/static/js/map.js
+++ map/static/js/map.js
@@ -4,22 +4,23 @@ require([
 ], function(Model, $) {
     $('.datepicker').pickadate({
         onOpen: function() {
-          var _this = this.$node;
-          if (_this.is('#start-date')) {
-              this.set('max', new Date($('.datepicker#end-date').val()));
-          } else if (_this.is('#end-date')) {
-              this.set('min', new Date($('.datepicker#start-date').val()));
-          }
+            var _this = this.$node;
+            var select = this.get('select');
+            if (select === null) {
+              this.set('select', new Date(_this.data('initial')));
+            }
+            var start = $('.datepicker#start-date');
+            var end = $('.datepicker#end-date');
+            var min = start.val() ? start.val() : start.data('initial');
+            var max = end.val() ? end.val() : end.data('initial');
+            this.set('max', new Date(max));
+            this.set('min', new Date(min));
         },
         onClose: function() {
             $(document.activeElement).blur();
         },
-        onStart: function() {
-          var initial = this.$node.data('initial');
-          this.set('select', new Date(initial));
-        },
-        min: new Date($('.datepicker#start-date').data('initial')),
-        max: new Date($('.datepicker#end-date').data('initial')),
+        // min: new Date($('.datepicker#start-date').data('initial')),
+        // max: new Date($('.datepicker#end-date').data('initial')),
         today: false,
         selectYears: 20,
         selectMonths: true,
