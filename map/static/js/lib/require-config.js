4260c0a437122528fef6040f598465a3ac6f1847
Jay Arnel Bilocura <jay.arnel@gmail.com>
2016-04-17 23:32:00 +0800

    added hammer js to materialize dependencies

--- map/static/js/lib/require-config.js
+++ map/static/js/lib/require-config.js
@@ -12,12 +12,14 @@
         goog: 'lib/plugins/goog',
         propertyParser: 'lib/plugins/propertyParser',
         stapes: 'lib/stapes-0.8.1-min',
+        hammerjs: 'lib/hammer.min',
+        'jquery-hammerjs': 'lib/jquery.hammer',
         materialize: 'materialize.min',
         model: 'model',
     },
     shim: {
         materialize: {
-            deps: ['jquery'],
+            deps: ['jquery', 'hammerjs', 'jquery-hammerjs'],
         },
         jquery: {
             exports: '$'
