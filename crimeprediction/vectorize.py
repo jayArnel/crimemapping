86f635e868bcc984eaa964268122c01c3fd38c1f
Jay Arnel Bilocura <jay.arnel@gmail.com>
2016-04-23 10:01:56 +0800

    better condition handling

--- crimeprediction/vectorize.py
+++ crimeprediction/vectorize.py
@@ -6,37 +6,13 @@ from crime.models import CriminalRecord
 
 def vectorize(grid_size, period, new=False):
     file = str(grid_size) + 'grid_size_' + period + 'period_vector.p'
-    if new:
-        try:
+    try:
+        if new:
             vectors = pickle.load(open(file, "rb"))
-        except (OSError, IOError) as e:
-            city = CityBorder.objects.get(name='Chicago')
-            grid = city.generateGrid(grid_size)
-            vectors = []
-            first_data = CriminalRecord.objects.first()
-            last_data = CriminalRecord.objects.last()
-            first_year = first_data.date.year
-            first_month = first_data.date.month
-            last_year = last_data.date.year
-            last_month = last_data.date.month
-            start = 12 * first_year + first_month - 1
-            end = 12 * last_year + last_month
-            for ym in range(start, end ):
-                year, month = divmod( ym, 12 )
-                month += 1
-                print year, month
-                vector = []
-                for i in xrange(len(grid)):
-                    g = grid[i]
-                    crimes = CriminalRecord.objects.filter(
-                        date__month=month, date__year=year,
-                        location__intersects=g).count()
-                    has_crime = int(crimes > 0)
-                    vector.append(has_crime)
-                vectors.append(vector)
-            pickle.dump(vectors, open(file, "wb"))
-        return vectors
-    else:
+        else:
+            raise EnvironmentError()
+    except EnvironmentError as e:
+        print "here"
         city = CityBorder.objects.get(name='Chicago')
         grid = city.generateGrid(grid_size)
         vectors = []
@@ -62,4 +38,4 @@ def vectorize(grid_size, period, new=False):
                 vector.append(has_crime)
             vectors.append(vector)
         pickle.dump(vectors, open(file, "wb"))
-        return vectors
+    return vectors
