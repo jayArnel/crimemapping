afb93fd433913cdda7d9909c8db01960a0232d9b
Jay Arnel Bilocura <jay.arnel@gmail.com>
2016-04-24 14:44:58 +0800

    separate method for monthly vectorization

--- crimeprediction/vectorize.py
+++ crimeprediction/vectorize.py
@@ -14,27 +14,31 @@ def vectorize(grid_size, period, new=False):
     except EnvironmentError as e:
         city = CityBorder.objects.get(name='Chicago')
         grid = city.generateGrid(grid_size)
-        vectors = []
-        first_data = CriminalRecord.objects.first()
-        last_data = CriminalRecord.objects.last()
-        first_year = first_data.date.year
-        first_month = first_data.date.month
-        last_year = last_data.date.year
-        last_month = last_data.date.month
-        start = 12 * first_year + first_month - 1
-        end = 12 * last_year + last_month
-        for ym in range(start, end ):
-            year, month = divmod( ym, 12 )
-            month += 1
-            print year, month
-            vector = []
-            for i in xrange(len(grid)):
-                g = grid[i]
-                crimes = CriminalRecord.objects.filter(
-                    date__month=month, date__year=year,
-                    location__intersects=g).count()
-                has_crime = int(crimes > 0)
-                vector.append(has_crime)
-            vectors.append(vector)
+        if period == 'monthly':
+            vectors = vectorize_monthly(grid)
         pickle.dump(vectors, open(file, "wb"))
     return vectors
+
+def vectorize_monthly(grid):
+    first_data = CriminalRecord.objects.first()
+    last_data = CriminalRecord.objects.last()
+    first_year = first_data.date.year
+    first_month = first_data.date.month
+    last_year = last_data.date.year
+    last_month = last_data.date.month
+    start = 12 * first_year + first_month - 1
+    end = 12 * last_year + last_month
+    for ym in range(start, end ):
+        year, month = divmod( ym, 12 )
+        month += 1
+        print year, month
+        vector = []
+        for i in xrange(len(grid)):
+            g = grid[i]
+            crimes = CriminalRecord.objects.filter(
+                date__month=month, date__year=year,
+                location__intersects=g).count()
+            has_crime = int(crimes > 0)
+            vector.append(has_crime)
+        vectors.append(vector)
+    return vector
