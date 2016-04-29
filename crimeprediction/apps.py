a6776cf61870d2feb1ff4d981685faaab639789a
Jay Arnel Bilocura <jay.arnel@gmail.com>
2016-04-29 15:12:58 +0800

    removed ready method in appconfig

--- crimeprediction/apps.py
+++ crimeprediction/apps.py
@@ -6,6 +6,3 @@ from django.apps import AppConfig
 class CrimePredictionConfig(AppConfig):
     name = 'crimeprediction'
     verbose_name = 'Crime Prediction'
-
-    def ready(self):
-        import crimeprediction.signals
