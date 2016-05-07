2a032e5ff7e8612e7f2c52fd35abc817203748f7
Jay Arnel Bilocura <jay.arnel@gmail.com>
2016-05-07 18:26:34 +0800

    removed unncessary plugins

--- map/static/js/lib/require/require-config.js
+++ map/static/js/lib/require/require-config.js
@@ -8,9 +8,6 @@
     baseUrl: '/static/js',
     paths: {
         jquery: 'lib/jquery/jquery-1.11.3.min',
-        async: 'lib/require/plugins/async',
-        goog: 'lib/require/plugins/goog',
-        propertyParser: 'lib/require/plugins/propertyParser',
         stapes: 'lib/stapes/stapes-0.8.1-min',
         hammerjs: 'lib/hammer/hammer.min',
         'jquery-hammerjs': 'lib/jquery/jquery.hammer',
