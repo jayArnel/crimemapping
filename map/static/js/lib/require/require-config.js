8a3f99477153c893c2b1c2c26e792a08cc29f935
Jay Arnel Bilocura <jay.arnel@gmail.com>
2016-05-07 18:08:27 +0800

    restructured lib

---
+++ map/static/js/lib/require/require-config.js
@@ -0,0 +1,28 @@
+(function(config) {
+    if (window.hasOwnProperty('require')) {
+        require.config(config);
+    } else {
+        window.require = config;
+    }
+})({
+    baseUrl: '/static/js',
+    paths: {
+        jquery: 'lib/jquery/jquery-1.11.3.min',
+        async: 'lib/require/plugins/async',
+        goog: 'lib/require/plugins/goog',
+        propertyParser: 'lib/require/plugins/propertyParser',
+        stapes: 'lib/stapes/stapes-0.8.1-min',
+        hammerjs: 'lib/hammer/hammer.min',
+        'jquery-hammerjs': 'lib/jquery/jquery.hammer',
+        materialize: 'lib/materialize/materialize.min',
+        model: 'model',
+    },
+    shim: {
+        materialize: {
+            deps: ['jquery', 'hammerjs', 'jquery-hammerjs'],
+        },
+        jquery: {
+            exports: '$'
+        }
+    }
+});
