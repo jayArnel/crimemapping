95c7a23b3e9c2f875f48f260f0a4e8d67cb7be1d
Jay Arnel Bilocura <jay.arnel@gmail.com>
2016-05-10 03:27:39 +0800

    added name as url argument

--- map/urls.py
+++ map/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url, include
 import views
 
 urlpatterns = [
-    url(r'^$', views.MapView.as_view(), name='home'),
+    url(r'^(?P<name>[\w-]+)/$', views.MapView.as_view(), name='map'),
     url(r'^grid/$', views.FetchGridView.as_view(), name='grid'),
 ]
