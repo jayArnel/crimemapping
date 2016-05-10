6bd1619debdc0ff3fa9d18624f4050d7759b8a4d
Jay Arnel Bilocura <jay.arnel@gmail.com>
2016-05-10 20:52:34 +0800

    removed custom templates dir in settings

--- crimemapping/settings.py
+++ crimemapping/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'crimemapping.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
