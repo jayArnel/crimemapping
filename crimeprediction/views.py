1fca4c376348e18a1ed2b4f4267d10852fed3bf5
Jay Arnel Bilocura <jay.arnel@gmail.com>
2016-05-24 22:57:03 +0800

    moved MapView from map to crimeprediction

--- crimeprediction/views.py
+++ crimeprediction/views.py
@@ -1,5 +1,23 @@
+from django.shortcuts import get_object_or_404
 from django.views.generic import TemplateView
+
+from crime.models import CriminalRecord
+from map.models import CityBorder
 
 
 class HomeView(TemplateView):
-    template_name = 'home.html'
+    template_name = 'home/home.html'
+
+
+class MapView(TemplateView):
+    template_name = 'map.html'
+
+    def get_context_data(self, **kwargs):
+        context = {}
+        city = get_object_or_404(CityBorder, name=kwargs['name'])
+        crimes = CriminalRecord.objects.all()
+        context['crime_types'] = crimes.order_by(
+            'primary_type').distinct().values_list('primary_type', flat=True)
+        context['start'] = str(crimes.order_by('date').first().date.date())
+        context['end'] = str(crimes.order_by('date').last().date.date())
+        return context
