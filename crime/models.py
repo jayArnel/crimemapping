29f11d6fdbb3b3f5add6c1bc936d1e6e1db970d9
Jay Arnel Bilocura <jay.arnel@gmail.com>
2016-06-05 05:38:53 +0800

    removed comment

--- crime/models.py
+++ crime/models.py
@@ -2,8 +2,6 @@ from __future__ import unicode_literals
 
 from django.contrib.gis.db import models
 
-# Create your models here.
-
 
 class CriminalRecord(models.Model):
     """ a model for a single criminal record """
