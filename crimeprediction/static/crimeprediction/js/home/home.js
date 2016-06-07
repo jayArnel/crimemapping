e171a526a2018335c0e41111ddeb3048e660f8a8
Jay Arnel Bilocura <jay.arnel@gmail.com>
2016-06-07 23:15:44 +0800

    show map modal with city items

--- crimeprediction/static/crimeprediction/js/home/home.js
+++ crimeprediction/static/crimeprediction/js/home/home.js
@@ -1,5 +1,23 @@
 require([
-    'jquery', 'hammerjs', 'jquery-hammerjs', 'materialize',
-], function($) {
-    $('.modal-trigger').leanModal();
+    'jquery', 'model', 'hammerjs', 'jquery-hammerjs', 'materialize',
+], function($, Model) {
+
+    $('.modal-trigger').on('click', populateCityPopup);
+
+    function populateCityPopup() {
+        var popup = $('#map-popup');
+        var popupContent = popup.find('.modal-content');
+        var CityBorder = new Model('cityborder-detail');
+
+        CityBorder.objects.filter({}, function(data) {
+            for (var i = 0; i < data.length; i++) {
+                var city = data[i];
+                var container = $('<div class="col s12 m6"><div class="card-panel"></div></div>');
+                var content = container.find('.card-panel').append($('<span>'+city.name+'</span>'));
+                popupContent.append(container);
+            }
+        });
+
+        popup.openModal();
+    }
 });
